7d4e22e61ec642d0993bc1270db2155a10e2b5b0
Hali <hali@trampolinebranding.com>
2022-09-18 14:07:13 -0230

    fixed layout

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -40,6 +40,13 @@ class Resume extends Component {
 
     return (
       <section id="resume">
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
+          
         <div className="row education">
           <div className="three columns header-col">
             <h1>
@@ -54,12 +61,7 @@ class Resume extends Component {
           </div>
         </div>
 
-        <div className="row work">
-          <div className="three columns header-col">
-            <h1>
-              <span>Work</span>
-            </h1>
-          </div>
+        
 
           <div className="nine columns main-col">{work}</div>
         </div>
