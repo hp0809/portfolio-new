1c1bfd58e57343262099a5e713dd6fc3390b8478
Hali Power <halipower@MacBook-Pro.local>
2023-01-16 15:31:57 -0330

    changed order of CV

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,18 +3,6 @@ import React, { Component } from "react";
 class Resume extends Component {
   render() {
     if (this.props.data) {
-      var education = this.props.data.education.map(function (education) {
-        return (
-          <div key={education.school}>
-            <h3>{education.school}</h3>
-            <p className="info">
-              {education.degree} <span>&bull;</span>
-              <em className="date">{education.graduated}</em>
-            </p>
-            <p>{education.description}</p>
-          </div>
-        );
-      });
       var work = this.props.data.work.map(function (work) {
         return (
           <div key={work.company}>
@@ -24,6 +12,18 @@ class Resume extends Component {
               <span>&bull;</span> <em className="date">{work.years}</em>
             </p>
             <p>{work.description}</p>
+          </div>
+        );
+      });
+      var education = this.props.data.education.map(function (education) {
+        return (
+          <div key={education.school}>
+            <h3>{education.school}</h3>
+            <p className="info">
+              {education.degree} <span>&bull;</span>
+              <em className="date">{education.graduated}</em>
+            </p>
+            <p>{education.description}</p>
           </div>
         );
       });
